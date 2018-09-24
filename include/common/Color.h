e580d9c1ef893a31b7222b7d3c9f441c23093a25
Guillem Orellana <guillem.orellana@gmail.com>
2018-09-24 13:15:08 +0200

    Redifined colors

--- include/common/Color.h
+++ include/common/Color.h
@@ -8,13 +8,12 @@ class Color
     SDL_Color m_color;
 
   public:
-    Color(Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha)
+    constexpr Color(Uint8 red, Uint8 green, Uint8 blue, Uint8 alpha) : m_color({red, green, blue, alpha})
     {
-        m_color = {red, green, blue, alpha};
     }
-    Color(Uint8 red, Uint8 green, Uint8 blue)
+
+    constexpr Color(Uint8 red, Uint8 green, Uint8 blue) : m_color({red, green, blue, 255})
     {
-        m_color = {red, green, blue, 255};
     }
 
     const SDL_Color &get() const { return m_color; }
@@ -23,11 +22,11 @@ class Color
 class Colors
 {
   public:
-    static const Color WHITE;
-    static const Color BLACK;
-    static const Color RED;
-    static const Color GREEN;
-    static const Color BLUE;
-    static const Color WHITESMOKE;
-    static const Color DARK;
+    static constexpr Color WHITE = Color(0, 0, 0);
+    static constexpr Color BLACK = Color(255, 255, 255);
+    static constexpr Color RED = Color(255, 0, 0);
+    static constexpr Color GREEN = Color(0, 255, 0);
+    static constexpr Color BLUE = Color(0, 0, 255);
+    static constexpr Color WHITESMOKE = Color(76, 76, 76);
+    static constexpr Color DARK = Color(235, 235, 235);
 };
