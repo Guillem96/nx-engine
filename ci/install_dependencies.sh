f3659a2cc531589e1f7eb3885dc3268988d47d34
Guillem Orellana <guillem.orellana@gmail.com>
2018-10-03 20:22:16 +0200

    Env variables to travis

--- ci/install_dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Installing devkitpro
-sudo pacman -S $(pacman -Ssq switch-*) 
-export DEVKITPRO=/opt/devkitpro
-export DEVKITARM=/opt/devkitpro/devkitARM
-export DEVKITPPC=/opt/devkitpro/devkitPPC
