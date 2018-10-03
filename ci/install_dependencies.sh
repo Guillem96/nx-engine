35a1f556bab9663c201d194c09230701fae1fee5
Guillem Orellana <guillem.orellana@gmail.com>
2018-10-03 20:07:46 +0200

    Install pacman on before_install

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -1,9 +1,4 @@
-#!/usr/bin/env sh
-
-# Intalling pacman
-wget -O "pacman.deb" https://github.com/devkitPro/pacman/releases/download/devkitpro-pacman-1.0.1/devkitpro-pacman.deb
-sudo dpkg  -i pacman.deb
-rm pacman.deb
+#!/bin/bash
 
 # Installing devkitpro
 sudo pacman -S $(pacman -Ssq switch-*) 
